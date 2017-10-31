745af649d81dcf8ee260b580e446a2a828e9804b
Mahgald <ricardo.surribas@gmail.com>
2017-10-31 18:14:37 -0300

    First Commit

---
+++ limits-services/src/main/java/com/course/microservice/limitsservices/LimitsServicesApplication.java
@@ -0,0 +1,12 @@
+package com.course.microservice.limitsservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServicesApplication.class, args);
+	}
+}
